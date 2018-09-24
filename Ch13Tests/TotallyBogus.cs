fd6403e588f9149e2be3c161ff6970a40b1786fb
jallen <jallen@snow.edu>
2018-09-24 11:10:45 -0600

    Got basics of testing working

---
+++ Ch13Tests/TotallyBogus.cs
@@ -0,0 +1,27 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Ch13.ViewModel;
+
+namespace Ch13Tests
+{
+    [TestFixture]
+    public class FirstTest
+    {
+        
+        [Test]
+        public void FirstTestMethod()
+        {
+            var vm = new MainViewModel();
+            
+        }
+
+        public void NotATest()
+        {
+
+        }
+    }
+}
