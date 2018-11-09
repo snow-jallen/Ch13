0589c71dd6062c81d2c2adc4567604f2f2474f41
jallen <jallen@snow.edu>
2018-11-09 10:45:23 -0700

    Not sure what al I've done but I did something.

---
+++ Ch13.Ooui/EmployeeManagementPageMaster.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Ch13.Ooui
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class EmployeeManagementPageMaster : ContentPage
+    {
+        public ListView ListView;
+
+        public EmployeeManagementPageMaster()
+        {
+            InitializeComponent();
+
+            BindingContext = new EmployeeManagementPageMasterViewModel();
+            ListView = MenuItemsListView;
+        }
+
+        class EmployeeManagementPageMasterViewModel : INotifyPropertyChanged
+        {
+            public ObservableCollection<EmployeeManagementPageMenuItem> MenuItems { get; set; }
+            
+            public EmployeeManagementPageMasterViewModel()
+            {
+                MenuItems = new ObservableCollection<EmployeeManagementPageMenuItem>(new[]
+                {
+                    new EmployeeManagementPageMenuItem { Id = 0, Title = "Page 1" },
+                    new EmployeeManagementPageMenuItem { Id = 1, Title = "Page 2" },
+                    new EmployeeManagementPageMenuItem { Id = 2, Title = "Page 3" },
+                    new EmployeeManagementPageMenuItem { Id = 3, Title = "Page 4" },
+                    new EmployeeManagementPageMenuItem { Id = 4, Title = "Page 5" },
+                });
+            }
+            
+            #region INotifyPropertyChanged Implementation
+            public event PropertyChangedEventHandler PropertyChanged;
+            void OnPropertyChanged([CallerMemberName] string propertyName = "")
+            {
+                if (PropertyChanged == null)
+                    return;
+
+                PropertyChanged.Invoke(this, new PropertyChangedEventArgs(propertyName));
+            }
+            #endregion
+        }
+    }
+}
---
+++ Ch13.Ooui/Program.cs
@@ -0,0 +1,22 @@
+﻿using Ooui;
+using System;
+using System.Diagnostics;
+using Xamarin.Forms;
+
+namespace Ch13.Ooui
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Forms.Init();
+            UI.Publish("/", new MainPage().GetOouiElement());
+#if DEBUG
+            UI.Port = 12345;
+            UI.Host = "localhost";
+            Process.Start("explorer", $"http://{UI.Host}:{UI.Port}");
+            Console.ReadLine();
+#endif
+        }
+    }
+}
